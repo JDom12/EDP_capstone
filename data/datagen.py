a8e1ab34898fec727a509d6ea9ab6afc046ed9dc
Nomi <nomi.fletcher1@gmail.com>
2025-03-18 15:57:13 -0400

    added phone column to data

--- data/datagen.py
+++ data/datagen.py
@@ -7,7 +7,7 @@ fake = Faker()
 data = list(range(1000))
 
 #generating fake data to use for both prediction model and to put into Postgres DB for employees
-data = [{"name": fake.name(), "location": fake.city(), "role": fake.job(), "salary": random.randint(10000, 100000), "manager_id": random.randint(1, 100)} for _ in data]
+data = [{"name": fake.name(), "phone": fake.phone_number(), "location": fake.city(), "role": fake.job(), "salary": random.randint(10000, 100000), "manager_id": random.randint(1, 100)} for _ in data]
 
 data = pd.DataFrame(data)
 
