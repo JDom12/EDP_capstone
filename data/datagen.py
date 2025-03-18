ba8a9a7fc069bb87b291b83036d23dcc2da3540d
Nomi <nomi.fletcher1@gmail.com>
2025-03-18 15:46:18 -0400

    Added data folder with data generation logic and sample data to import to Postgres

---
+++ data/datagen.py
@@ -0,0 +1,16 @@
+import random
+from faker import Faker
+import pandas as pd
+
+fake = Faker()
+
+data = list(range(1000))
+
+#generating fake data to use for both prediction model and to put into Postgres DB for employees
+data = [{"name": fake.name(), "location": fake.city(), "role": fake.job(), "salary": random.randint(10000, 100000), "manager_id": random.randint(1, 100)} for _ in data]
+
+data = pd.DataFrame(data)
+
+#creates csv file
+data.to_csv('sample_data', index=False)
+
