66ec513f445f87b63559bfdac83f70696b2193d3
JDom12 <dominguezjulian12@gmail.com>
2025-03-19 13:07:43 -0400

    add routes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,29 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './assets/Login'
-import Emp from './assets/Emp'
-import Search from './assets/Search'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import Login from './assets/Login';
+import Emp from './assets/Emp';
+import Search from './assets/Search';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-      <h1>Enterprise Directory</h1>
-      <Login/>
-      <Emp/>
-      <Search/>
-    </>
-  )
+    <Router>
+      <div>
+        <h1>Enterprise Directory</h1>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/emp" element={<Emp />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
