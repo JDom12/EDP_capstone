24011290bf6c81c93a0288fea3e527f67f7d716a
Nomi <nomi.fletcher1@gmail.com>
2025-03-19 10:47:06 -0400

    added hooks to login

--- react/src/assets/Login.jsx
+++ react/src/assets/Login.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
 //login form here
+//const navigate = useNavigate();
 
-const navigate = useNavigate();
+const [id, setId] = useState('');
+const [pass, setPass] = useState('');
+
+const handleIdChange = (e) => {
+    setId(e.target.value);
+    console.log(id);
+};
+
+const handlePassChange = (e) => {
+    setPass(e.target.value);
+    console.log(pass);
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
+  console.log(id, pass);
 
-  navigate('/search');
+  //navigate('/search');
 };
 
 return (
@@ -17,10 +30,10 @@ return (
         <div>
         <form>
             <label for="id">Employee ID </label>
-            <input type="text" id='id'/><br/>
+            <input type="text" id='id' value={id} onChange={handleIdChange}/><br/>
             <label for="pass">Password </label>
-            <input type="text" id='pass'/><br/>
-            <input type="submit" value="Submit"/>
+            <input type="text" id='pass' value={pass} onChange={handlePassChange}/><br/>
+            <input type="submit" value="Submit" onSubmit={handleSubmit}/>
         </form>
         </div>
     </>
