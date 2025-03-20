c64b3926053d827fae780812cd50dacf367a678e
JDom12 <dominguezjulian12@gmail.com>
2025-03-20 11:12:36 -0400

    navBar

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css';
 import Login from './assets/Login';
 import Emp from './assets/Emp';
@@ -8,21 +8,33 @@ import ProtectedRoute from "./assets/RequireAuth";
 import SalaryPredictor from "./assets/SalaryPredictor";
 
 function App() {
+
   return (
   <AuthProvider>
     <h1>Enterprise Directory</h1>
     <Router>
+          <nav className="navBar">
+            <ul className="navLinks">
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/predict-salary">Predict Salary</Link>
+              </li>
+            </ul>
+          </nav>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route element={<ProtectedRoute />}>
-            <Route path="/emp" element={<Emp />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/predict-salary" element={<SalaryPredictor />} />
+          <Route path="/emp" element={<Emp />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/predict-salary" element={<SalaryPredictor />} />
         </Route>
         </Routes>
     </Router>
     </AuthProvider>
   );
 }
+
 
 export default App;
